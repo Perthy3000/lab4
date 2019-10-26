29a711a1ea2e24cbb5252063c6c21298ff9e741d
Perth <perth.ploy@gmail.com>
2019-10-26 13:22:31 +0700

    Finish Drill

--- src/building/Drill.java
+++ src/building/Drill.java
@@ -1,19 +1,20 @@
 package building;
 
-import building.base.Building;
+import building.base.ElectricBuilding;
 import building.base.ItemProducer;
 import item.Item;
 import item.ItemType;
 import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
+import logic.GameState;
 import ui.BuildingIcon;
 import ui.ItemIcon;
 
-public class Drill extends Building implements ItemProducer {
+public class Drill extends ElectricBuilding implements ItemProducer {
 
 	private static final int HARVEST_INTERVAL = 4;
 	private static final int ENERGY_CONSUMPTION = 5;
-	
+	private int currentCycle = 0;
 
 	@Override
 	public void render(StackPane target) {
@@ -21,12 +22,42 @@ public class Drill extends Building implements ItemProducer {
 		target.getChildren().add(icon);
 		
 		// TODO: fill this boolean
-		boolean readyToProduceIron;		// is this drill ready to produce iron?
+		boolean readyToProduceIron = canProduceItem();		// is this drill ready to produce iron?
 		
 		if(readyToProduceIron) {
 			ItemIcon itemIcon = ItemType.IRON.toItemIcon();
 			StackPane.setAlignment(itemIcon, Pos.TOP_CENTER);
 			target.getChildren().add(itemIcon);
 		}
+	}
+
+	@Override
+	public void operate() {
+		if(consumeElectricity() && currentCycle < HARVEST_INTERVAL) {
+			currentCycle++;
+		}
+	}
+
+	@Override
+	public boolean canProduceItem() {
+		if(currentCycle == HARVEST_INTERVAL) {
+			return true;
+		}
+		return false;
+	}
+
+
+	@Override
+	public Item getProducedItem() {
+		currentCycle = 0;
+		return Item.iron();
+	}
+
+	@Override
+	protected boolean consumeElectricity() {
+		if(GameState.instance.consumeElectricity(ENERGY_CONSUMPTION)) {
+			return true;
+		}
+		return false;
 	}
 }
