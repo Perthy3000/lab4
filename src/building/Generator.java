3f1e500e49a947a0dc14a75c259ffb504e4cbc9e
Perth <perth.ploy@gmail.com>
2019-10-26 12:08:45 +0700

    Finish generator

--- src/building/Generator.java
+++ src/building/Generator.java
@@ -14,6 +14,8 @@ public class Generator extends Building implements ItemReceiver {
 	
 	private static final int GENERATION_DELAY = 3;
 	private static final int GENERATED_ELECTRICITY = 5;
+	private int currentCycle = 0;
+	private boolean hasWood = false;
 
 	@Override
 	public void render(StackPane target) {
@@ -21,12 +23,41 @@ public class Generator extends Building implements ItemReceiver {
 		target.getChildren().add(icon);
 		
 		// TODO: fill this boolean
-		boolean hasWood;		// does this generator has wood in it?
+		boolean hasWood = this.hasWood;		// does this generator has wood in it?
 		
 		if(hasWood) {
 			ItemIcon itemIcon = ItemType.WOOD.toItemIcon();
 			StackPane.setAlignment(itemIcon, Pos.TOP_CENTER);
 			target.getChildren().add(itemIcon);
+		}
+	}
+	
+	@Override
+	public void operate() {
+		if(hasWood) {
+			currentCycle++;
+		}
+		generateElectricity();
+	}
+
+	@Override
+	public boolean canReceiveItem(ItemType oftype) {
+		if(!hasWood && oftype == ItemType.WOOD) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void receiveItem(Item item) {
+		hasWood = true;
+	}
+	
+	private void generateElectricity() {
+		if(currentCycle == GENERATION_DELAY) {
+			GameState.instance.generateElectricity(GENERATED_ELECTRICITY);
+			currentCycle = 0;
+			hasWood = false;
 		}
 	}
 
